6843ddf5a2ae08e48fc03ec1e0c21ad4866b401b
takeru.ota <takeru.ota@togawa.cs.waseda.ac.jp>
2023-06-05 21:10:18 +0900

    edit file for test (not complete)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
 import TimeTableFrame from "@/components/timetable/TimeTableFrame"
+import TimeTablePeriodLabel from "@/components/timetable/TimeTablePeriodLabel"
+import { ClientDataProvider } from "@/hooks/ClientDataContext"
+import { ClientDataModel } from "@/models/ClientDataModel"
 
-export default function Home() {
+const getClientData = async (): Promise<ClientDataModel> => {
+  const res = await fetch("http://localhost:3000/initial_client_data.json")
+  const data = await res.json()
+  return data
+}
+
+const Home = async () => {
+  const initialClientData = await getClientData()
+
   return (
     <main>
-      <TimeTableFrame />
+      <ClientDataProvider initialClientData={initialClientData}>
+        <TimeTableFrame />
+      </ClientDataProvider>
     </main>
   )
 }
+
+export default Home
