2427099e74fe75337ca76fbf8d82b97241474b21
takeru.ota <takeru.ota@togawa.cs.waseda.ac.jp>
2023-06-07 16:53:35 +0900

    add 'fade-in' animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,28 @@ module.exports = {
         sans: ["var(--font-noto-sans-jp)"],
         roboto: ["var(--font-roboto)"],
       },
+      animation: {
+        "fade-in": "fade-in 0.3s cubic-bezier(0.390, 0.575, 0.565, 1.000) both",
+        "fade-in-1/2": "fade-in-1/2 0.3s cubic-bezier(0.390, 0.575, 0.565, 1.000) both",
+      },
+      keyframes: {
+        "fade-in": {
+          "0%": {
+            opacity: "0",
+          },
+          to: {
+            opacity: "1",
+          },
+        },
+        "fade-in-1/2": {
+          from: {
+            opacity: "0",
+          },
+          to: {
+            opacity: "0.5",
+          },
+        },
+      },
     },
   },
   plugins: [],
