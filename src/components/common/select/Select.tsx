da05af6d5f526fb899b30080771e499707eae084
takeru.ota <takeru.ota@togawa.cs.waseda.ac.jp>
2023-06-09 13:06:06 +0900

    edit Select components

--- src/components/common/select/Select.tsx
+++ src/components/common/select/Select.tsx
@@ -1,37 +1,49 @@
 import { ChevronDownIcon, ChevronUpIcon } from "@radix-ui/react-icons"
 import * as SelectPrimitive from "@radix-ui/react-select"
+import classNames from "classnames"
 import { ReactNode, forwardRef } from "react"
+import { UseFormRegisterReturn } from "react-hook-form"
 
-interface SelectProps extends SelectPrimitive.SelectProps {
+interface SelectProps extends UseFormRegisterReturn {
   className?: string
   children?: ReactNode
+  defaultValue?: string
 }
 
 export const Select = forwardRef<HTMLButtonElement, SelectProps>(function Select(
-  { className, children, ...props },
+  { className, children, name, onChange, required, disabled, defaultValue },
   ref,
 ) {
   return (
-    <SelectPrimitive.Root {...props}>
+    <SelectPrimitive.Root
+      name={name}
+      onValueChange={(value) => onChange({ target: { name, value } })}
+      required={required}
+      disabled={disabled}
+      defaultValue={defaultValue}
+    >
       <SelectPrimitive.Trigger
-        className="inline-flex h-9 w-full items-center rounded border-[1px] border-blue-500 p-2 text-sm text-gray-800 focus:border-2"
+        className={classNames(
+          className,
+          "inline-flex h-9 w-full items-center justify-between gap-2 rounded border-[1px] border-blue-500 p-2 text-sm text-gray-800 focus:border-2",
+        )}
         ref={ref}
       >
-        <SelectPrimitive.Value className="" placeholder={props.defaultValue} />
-        <SelectPrimitive.Icon>
+        <SelectPrimitive.Value />
+        <SelectPrimitive.Icon className="text-blue-700">
           <ChevronDownIcon />
         </SelectPrimitive.Icon>
       </SelectPrimitive.Trigger>
 
       <SelectPrimitive.Portal>
-        <SelectPrimitive.Content>
-          <SelectPrimitive.ScrollUpButton>
+        <SelectPrimitive.Content className="overflow-hidden rounded-md bg-white shadow-lg shadow-gray-700/30">
+          <SelectPrimitive.ScrollUpButton className="flex h-6 cursor-default items-center justify-center bg-white text-blue-700">
             <ChevronUpIcon />
           </SelectPrimitive.ScrollUpButton>
 
-          <SelectPrimitive.Viewport>{children}</SelectPrimitive.Viewport>
+          <SelectPrimitive.Viewport className="p-2">{children}</SelectPrimitive.Viewport>
 
-          <SelectPrimitive.ScrollDownButton>
+          <SelectPrimitive.ScrollDownButton className="flex h-6 cursor-default items-center justify-center bg-white text-blue-700">
             <ChevronDownIcon />
           </SelectPrimitive.ScrollDownButton>
         </SelectPrimitive.Content>
--- src/components/common/select/SelectItem.tsx
+++ src/components/common/select/SelectItem.tsx
@@ -1,5 +1,6 @@
 import { CheckIcon } from "@radix-ui/react-icons"
 import * as SelectPrimitive from "@radix-ui/react-select"
+import classNames from "classnames"
 import { ReactNode, forwardRef } from "react"
 
 interface SelectItemProps extends SelectPrimitive.SelectItemProps {
@@ -13,9 +14,16 @@ export const SelectItem = forwardRef<HTMLDivElement, SelectItemProps>(function S
   forwardedRef,
 ) {
   return (
-    <SelectPrimitive.Item className={className} {...props} ref={forwardedRef}>
+    <SelectPrimitive.Item
+      className={classNames(
+        className,
+        "relative flex h-6 select-none items-center rounded px-6 py-2 text-xs text-blue-700 data-[highlighted]:bg-blue-400 data-[highlighted]:text-white data-[highlighted]:outline-none md:text-sm",
+      )}
+      {...props}
+      ref={forwardedRef}
+    >
       <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
-      <SelectPrimitive.ItemIndicator>
+      <SelectPrimitive.ItemIndicator className="absolute left-0 inline-flex w-6 items-center justify-center">
         <CheckIcon />
       </SelectPrimitive.ItemIndicator>
     </SelectPrimitive.Item>
