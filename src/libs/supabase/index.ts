078912c1ca5c6f62f6a9fd84e0e404c1a0310fe2
takeru.ota <takeru.ota@togawa.cs.waseda.ac.jp>
2023-07-24 23:44:16 +0900

    make supabase templete

---
+++ src/libs/supabase/index.ts
@@ -0,0 +1,2 @@
+export { supabase } from './apiClient'
+export type { Query } from './type'
---
+++ src/libs/supabase/type.ts
@@ -0,0 +1,14 @@
+export type Query = {
+  year?: Array<number>
+  term?: Array<number>
+  day?: Array<number>
+  period?: Array<number>
+  faculty?: Array<number>
+  courseCategoryJp?: Array<string>
+  courseCategoryEn?: Array<string>
+  creditCategoryJp?: Array<string>
+  crefitCategoryEn?: Array<string>
+  campusCode?: Array<number>
+  eligibleYear?: Array<number>
+  keyword?: string
+}
