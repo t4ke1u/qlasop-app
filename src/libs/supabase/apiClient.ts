c6d2418975b432b40d5767b6a1f11d4b05b80e9f
takeru.ota <takeru.ota@togawa.cs.waseda.ac.jp>
2023-10-29 17:41:05 +0900

    rename SUPABASE environment variables

--- src/libs/supabase/apiClient.ts
+++ src/libs/supabase/apiClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+const supabaseUrl = process.env.SUPABASE_URL as string
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY as string
 
 const options = {
   auth: {
