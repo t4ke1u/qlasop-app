8687a7de3e19533cc96f212c12b00b3ba16ebe19
t4ke1u <bloomington.47408@gmail.com>
2024-06-08 14:27:47 +0900

    fix: lint error

--- src/app/api/courses/query/route.ts
+++ src/app/api/courses/query/route.ts
@@ -21,53 +21,71 @@ export const GET = async () => {
   const { data: mainLangData } = await supabase.from('course-main-language').select('id, jp')
   const { data: eligibleYearData } = await supabase.from('course-eligible-year').select('id')
 
-  const years: CourseQueryItem[] = yearData!.map((d) => ({ display: `${d.id}年度`, value: d.id }))
-  const faculties: CourseQueryItem[] = facultyData!
-    .map((d) => ({ display: d.jp, value: d.id }))
-    .sort((a, b) => a.value - b.value)
+  const years: CourseQueryItem[] = !yearData
+    ? []
+    : yearData.map((d) => ({ display: `${d.id}年度`, value: d.id }))
+  const faculties: CourseQueryItem[] = !facultyData
+    ? []
+    : facultyData.map((d) => ({ display: d.jp, value: d.id })).sort((a, b) => a.value - b.value)
   faculties.unshift({ display: '', value: '' })
   const courseCategories: { [key: string]: CourseQueryItem[] } = {}
-  courseCategoryData?.forEach(({ id, faculty_id, jp }) => {
-    if (courseCategories[faculty_id]) {
-      courseCategories[faculty_id].push({ display: jp, value: id })
-    } else {
-      courseCategories[faculty_id] = [
-        { display: '', value: '' },
-        { display: jp, value: id },
-      ]
-    }
-  })
+  if (!!courseCategoryData) {
+    courseCategoryData.forEach(({ id, faculty_id, jp }) => {
+      if (courseCategories[faculty_id]) {
+        courseCategories[faculty_id].push({ display: jp, value: id })
+      } else {
+        courseCategories[faculty_id] = [
+          { display: '', value: '' },
+          { display: jp, value: id },
+        ]
+      }
+    })
+  }
   const creditCategories: { [key: string]: CourseQueryItem[] } = {}
-  creditCategoryData?.forEach(({ id, faculty_id, jp }) => {
-    if (creditCategories[faculty_id]) {
-      creditCategories[faculty_id].push({ display: jp, value: id })
-    } else {
-      creditCategories[faculty_id] = [
-        { display: '', value: '' },
-        { display: jp, value: id },
-      ]
-    }
-  })
-  const terms: CourseQueryItem[] = termData!.map((d) => ({ display: d.jp, value: d.id }))
+  if (!!creditCategoryData) {
+    creditCategoryData.forEach(({ id, faculty_id, jp }) => {
+      if (creditCategories[faculty_id]) {
+        creditCategories[faculty_id].push({ display: jp, value: id })
+      } else {
+        creditCategories[faculty_id] = [
+          { display: '', value: '' },
+          { display: jp, value: id },
+        ]
+      }
+    })
+  }
+  const terms: CourseQueryItem[] = !termData
+    ? []
+    : termData.map((d) => ({ display: d.jp, value: d.id }))
   terms.unshift({ display: '', value: '' })
-  const days: CourseQueryItem[] = dayData!.map((d) => ({ display: d.jp, value: d.id }))
+  const days: CourseQueryItem[] = !dayData
+    ? []
+    : dayData.map((d) => ({ display: d.jp, value: d.id }))
   days.unshift({ display: '', value: '' })
-  const periods: CourseQueryItem[] = periodData!.map((d) => ({ display: d.jp, value: d.id }))
+  const periods: CourseQueryItem[] = !periodData
+    ? []
+    : periodData.map((d) => ({ display: d.jp, value: d.id }))
   periods.unshift({ display: '', value: '' })
-  const campuses: CourseQueryItem[] = campusData!.map((d) => ({
-    display: d.jp,
-    value: d.id,
-  }))
+  const campuses: CourseQueryItem[] = !campusData
+    ? []
+    : campusData.map((d) => ({
+        display: d.jp,
+        value: d.id,
+      }))
   campuses.unshift({ display: '', value: '' })
-  const mainLangs: CourseQueryItem[] = mainLangData!.map((d) => ({
-    display: d.jp,
-    value: d.id,
-  }))
+  const mainLangs: CourseQueryItem[] = !mainLangData
+    ? []
+    : mainLangData.map((d) => ({
+        display: d.jp,
+        value: d.id,
+      }))
   mainLangs.unshift({ display: '', value: '' })
-  const eligibleYears: CourseQueryItem[] = eligibleYearData!.map((d) => ({
-    display: `${d.id}年以上`,
-    value: d.id,
-  }))
+  const eligibleYears: CourseQueryItem[] = !eligibleYearData
+    ? []
+    : eligibleYearData.map((d) => ({
+        display: `${d.id}年以上`,
+        value: d.id,
+      }))
   eligibleYears.unshift({ display: '', value: '' })
 
   return NextResponse.json({
